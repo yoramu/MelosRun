2fa5c24865cd9103da1b1098fe163d236de8b295
西村卓也 <>
2019-02-06 06:16:50 +0900

    重力地形判定実装済み　ジャンプ開発中

---
+++ Assets/Script/PlayerMoveScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position += new Vector3 (Input.GetAxis ("Horizontal") * Time.deltaTime * 3, 0.0f, 0.0f);
+    }
+}
