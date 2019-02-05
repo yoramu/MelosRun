d6bd57432ac6dcdef9b950008ffb1519ef248c6d
西村卓也 <>
2019-02-06 06:16:50 +0900

    追跡カメラ実装 (+4 squashed commits) Squashed commits: [9042484] Standard Assetsをインポートした [a161db0] テストステージを色をつけて編集した [5a2eeff] ジャンプ実装済み（他オブジェクトへのめり込みを利用しての上方速度溜めバグ有り） [2fa5c24] 重力地形判定実装済み　ジャンプ開発中

---
+++ Assets/Script/ObstacleMoveScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMoveScript : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () { }
+
+    // Update is called once per frame
+    void Update () {
+        //if (transform.position.y < 10) {
+        //transform.position.y += 2
+        //} else {
+        //  transform.position.y += -2
+        //}
+    }
+}
---
+++ Assets/Script/PlayerMoveScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour {
+    public GameObject Player;
+    private Rigidbody PlayerRigid;
+
+    private bool isJumping = true;
+    public float Upspeed; //ジャンプのスピード
+    // Start is called before the first frame update
+    void Start () {
+        PlayerRigid = Player.GetComponent<Rigidbody> ();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        PlayerRigid.position += new Vector3 (Input.GetAxis ("Horizontal") * Time.deltaTime * 3, 0.0f, 0.0f);
+        if (Input.GetKeyDown (KeyCode.Space) && isJumping) {
+            PlayerRigid.AddForce (0f, Upspeed, 0f);
+        }
+        Debug.Log (isJumping);
+    }
+    private void OnCollisionExit (Collision collision) {
+        isJumping = false;
+    }
+    private void OnCollisionEnter (Collision collision) {
+        isJumping = true;
+    }
+}
