a161db06b3b96d0371acd5d15863125542bf2269
西村卓也 <>
2019-02-06 19:43:02 +0900

    testStage編集

--- Assets/Script/PlayerMoveScript.cs
+++ Assets/Script/PlayerMoveScript.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerMoveScript : MonoBehaviour {
     public GameObject Player;
-    private Rigidbody PlayerRigid; //PlayerオブジェクトのRigidbobyを保管する
+    private Rigidbody PlayerRigid;
 
     private bool isJumping = true;
-    public float Upspeed;　　　　 //ジャンプのスピード
+    public float Upspeed; //ジャンプのスピード
     // Start is called before the first frame update
     void Start () {
         PlayerRigid = Player.GetComponent<Rigidbody> ();
@@ -16,10 +16,13 @@ public class PlayerMoveScript : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         PlayerRigid.position += new Vector3 (Input.GetAxis ("Horizontal") * Time.deltaTime * 3, 0.0f, 0.0f);
-        if (Input.GetKeyDown (KeyCode.Space) && isJumping) {　　　　　　 //上にジャンプする。
+        if (Input.GetKeyDown (KeyCode.Space) && isJumping) {
             PlayerRigid.AddForce (0f, Upspeed, 0f);
-            isJumping = false;
         }
+        Debug.Log (isJumping);
+    }
+    private void OnCollisionExit (Collision collision) {
+        isJumping = false;
     }
     private void OnCollisionEnter (Collision collision) {
         isJumping = true;
