5a2eefffd56a4fd2d027e71b33552fade21d3208
西村卓也 <>
2019-02-06 11:56:43 +0900

    ジャンプ実装済み（他オブジェクトへのめり込みを利用しての上方速度溜めバグ有り）

--- Assets/Script/PlayerMoveScript.cs
+++ Assets/Script/PlayerMoveScript.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMoveScript : MonoBehaviour {
+    public GameObject Player;
+    private Rigidbody PlayerRigid; //PlayerオブジェクトのRigidbobyを保管する
+
+    private bool isJumping = true;
+    public float Upspeed;　　　　 //ジャンプのスピード
     // Start is called before the first frame update
     void Start () {
-
+        PlayerRigid = Player.GetComponent<Rigidbody> ();
     }
 
     // Update is called once per frame
     void Update () {
-        transform.position += new Vector3 (Input.GetAxis ("Horizontal") * Time.deltaTime * 3, 0.0f, 0.0f);
+        PlayerRigid.position += new Vector3 (Input.GetAxis ("Horizontal") * Time.deltaTime * 3, 0.0f, 0.0f);
+        if (Input.GetKeyDown (KeyCode.Space) && isJumping) {　　　　　　 //上にジャンプする。
+            PlayerRigid.AddForce (0f, Upspeed, 0f);
+            isJumping = false;
+        }
+    }
+    private void OnCollisionEnter (Collision collision) {
+        isJumping = true;
     }
 }
