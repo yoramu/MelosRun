f609cde51464fba96f98f73b661716d92699d920
西村卓也 <>
2019-03-03 07:06:43 +0900

    ジャンプフラグの大幅改善、ダブルジャンプ追加済み

---
+++ Assets/Script/JumpColliderScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpColliderScript : MonoBehaviour {
+    public int jumpCount = 0;
+    // Start is called before the first frame update
+    public void OnTriggerEnter (Collider other) {
+        jumpCount = 0;
+    }
+}
--- Assets/Script/PlayerMoveScript.cs
+++ Assets/Script/PlayerMoveScript.cs
@@ -3,28 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMoveScript : MonoBehaviour {
+    public GameObject footCollider;
     public GameObject Player;
+    public const int MaxJumpCount = 2;
+    public bool isJumping = false;
+
     private Rigidbody PlayerRigid;
 
-    private bool isJumping = true;
     public float Upspeed; //ジャンプのスピード
     // Start is called before the first frame update
     void Start () {
         PlayerRigid = Player.GetComponent<Rigidbody> ();
+        footCollider = transform.GetChild (0).gameObject;
     }
 
     // Update is called once per frame
     void Update () {
-        PlayerRigid.position += new Vector3 (Input.GetAxis ("Horizontal") * Time.deltaTime * 3, 0.0f, 0.0f);
-        if (Input.GetKeyDown (KeyCode.Space) && isJumping) {
-            PlayerRigid.AddForce (0f, Upspeed, 0f);
+        JumpColliderScript j = footCollider.GetComponent<JumpColliderScript> ();
+        PlayerRigid.position += new Vector3 (Input.GetAxis ("Horizontal") * Time.deltaTime * 5, 0.0f, 0.0f);
+
+        if (j.jumpCount < MaxJumpCount && Input.GetKeyDown (KeyCode.Space)) {
+            isJumping = true;
         }
-        Debug.Log (isJumping);
-    }
-    private void OnCollisionExit (Collision collision) {
-        isJumping = false;
-    }
-    private void OnCollisionEnter (Collision collision) {
-        isJumping = true;
+        if (isJumping) {
+            PlayerRigid.velocity = Vector3.zero;
+            PlayerRigid.AddForce (0f, Upspeed, 0f);
+            j.jumpCount++;
+            isJumping = false;
+        }
     }
 }
