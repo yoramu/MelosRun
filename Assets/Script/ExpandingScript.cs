5e90126bc50a84b7aa359457c082a6574d7af20d
yoramu <yoramuu@gmail.com>
2019-03-03 17:09:52 +0900

    Implement ExpandingCube

---
+++ Assets/Script/ExpandingScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandingScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private bool flag = true;
+    private float para = 0.05f;
+    private float interval = 0.05f;    
+    private float tmpTime1 = 0f; 
+    private float tmpTime2 = 0f; 
+    private int stackFlag = 0;
+    private bool smallFlag = true;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {           
+        if (smallFlag)
+        {
+            tmpTime2 += Time.deltaTime;
+            if(tmpTime2 >= interval){
+                if(transform.localScale.y > 1.1){
+                    transform.localScale += new Vector3(0f, -0.5f, 0f);
+                }
+                if(transform.localScale.y < 1.0){
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+                tmpTime2 = 0;
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "JumpCollider")
+        {
+            smallFlag = false;
+            tmpTime1 += Time.deltaTime;
+            if(tmpTime1 >= interval){
+                if(stackFlag > 40){
+                        transform.localScale += new Vector3(0f, 0.5f, 0f);
+                }
+                transform.localScale += new Vector3(para, para, para);
+                para *= -1f;
+                stackFlag += 1;
+                tmpTime1 = 0;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "JumpCollider")
+        {
+            smallFlag = true;
+        }
+    }
+}
