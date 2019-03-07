fa293a804a7bd8a234a4c590f70fdb2504f90d91
yoramu <yoramuu@gmail.com>
2019-03-07 19:43:13 +0900

    Implemented ContinueButton

---
+++ Assets/Script/ContinueButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContinueButton : MonoBehaviour
+{
+    public void ReStartGame(){
+        SceneManager.LoadScene("MainGameScene");
+    }    
+}
