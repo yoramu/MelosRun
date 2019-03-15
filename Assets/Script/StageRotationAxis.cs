733386bb86e5b6b802a2fa94ff6c931bd3374ca0
yoramu <yoramuu@gmail.com>
2019-03-15 22:18:33 +0900

    StageRotationAxis実装。前進方向変更して立体的にステージを移動可能。

--- Assets/Script/StageRotationAxis.cs
+++ Assets/Script/StageRotationAxis.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StageRotationAxis : MonoBehaviour {
-    // Start is called before the first frame update
+    private float direction;
+    private PlayerMove PlayerMove;
+    public GameObject SRA;
+    public GameObject StageGroup;
+    private float s;
     void Start () {
-        GameObject.Find ("StageGroup").transform.parent = transform;
+        SRA = GameObject.Find ("StageRotationAxis");
+        StageGroup = GameObject.Find ("StageGroup");
     }
+    void Update () { }
 
-    // Update is called once per frame
-    void Update () {
-        GameObject.Find ("StageGroup").transform.parent = transform;
-        GameObject.Find ("StageGroup").transform.parent = null;
+    private void OnTriggerStay (Collider other) {
+        if (other.gameObject.CompareTag ("Player")) {
+            StageGroup.transform.parent = SRA.transform;
+            float y = StageGroup.transform.rotation.eulerAngles.y;
+            PlayerMove = other.GetComponent<PlayerMove> ();
+            direction = PlayerMove.direction;
+
+            if (direction > 0) {
+                if (0 <= y && y < 90) {
+                    PlayerMove.IsMoveFalse ();
+                    SRA.transform.Rotate (new Vector3 (0, 1, 0));
+                } else {
+                    PlayerMove.IsMoveTrue ();
+                }
+            } else if (direction < 0) {
+                if (1 < y && y < 91) {
+                    PlayerMove.IsMoveFalse ();
+                    SRA.transform.Rotate (new Vector3 (0, -1, 0));
+                } else {
+                    PlayerMove.IsMoveTrue ();
+                }
+            }
+            GameObject.Find ("StageGroup").transform.parent = null;
+
+        }
     }
 }
