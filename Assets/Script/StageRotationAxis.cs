4fb8e2d1ec3d566fabf14e4313d15896ff897c0c
yoramu <yoramuu@gmail.com>
2019-03-16 03:42:11 +0900

    no message

--- Assets/Script/StageRotationAxis.cs
+++ Assets/Script/StageRotationAxis.cs
@@ -16,22 +16,23 @@ public class StageRotationAxis : MonoBehaviour {
 
     private void OnTriggerStay (Collider other) {
         if (other.gameObject.CompareTag ("Player")) {
+            transform.localScale = new Vector3 (10, 2, 10);
             StageGroup.transform.parent = transform;
             float y = StageGroup.transform.rotation.eulerAngles.y;
             PlayerMove = other.GetComponent<PlayerMove> ();
             direction = PlayerMove.direction;
 
             if (direction > 0) {
-                if (startAngle <= y && y < lastAngle) {
+                if (startAngle < y && y < lastAngle) {
                     PlayerMove.IsMoveFalse ();
-                    transform.Rotate (new Vector3 (0, 1, 0));
+                    transform.Rotate (new Vector3 (0, 2, 0));
                 } else {
                     PlayerMove.IsMoveTrue ();
                 }
             } else if (direction < 0) {
                 if (startAngle + 1 < y && y < lastAngle + 1) {
                     PlayerMove.IsMoveFalse ();
-                    transform.Rotate (new Vector3 (0, -1, 0));
+                    transform.Rotate (new Vector3 (0, 2, 0));
                 } else {
                     PlayerMove.IsMoveTrue ();
                 }
@@ -39,5 +40,8 @@ public class StageRotationAxis : MonoBehaviour {
             GameObject.Find ("StageGroup").transform.parent = null;
 
         }
+    }
+    private void OnTriggerExit (Collider other) {
+        transform.localScale = new Vector3 (0.1f, 2, 0.1f);
     }
 }
