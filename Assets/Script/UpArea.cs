29dfdb17ee195c192562a0197ec675c0211e5c42
yoramu <yoramuu@gmail.com>
2019-03-16 13:44:19 +0900

    UpAreaの風を強くした

--- Assets/Script/UpArea.cs
+++ Assets/Script/UpArea.cs
@@ -9,7 +9,7 @@ public class UpArea : MonoBehaviour {
     }
     private void OnTriggerStay (Collider other) {
         if (other.gameObject.CompareTag ("Player")) {
-            other.attachedRigidbody.AddForce (new Vector3 (0f, 0.1f, 0f), ForceMode.Impulse);
+            other.attachedRigidbody.AddForce (new Vector3 (0f, 1f, 0f), ForceMode.Impulse);
         }
     }
 }
