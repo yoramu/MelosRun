03972c52611e75c24af8d6392a80162b18085ce5
yoramu <yoramuu@gmail.com>
2019-03-18 08:44:37 +0900

    プレイヤーが円周に回るスクリプトを実装

--- Assets/Script/StageRotationPivotAxis.cs
+++ Assets/Script/StageRotationPivotAxis.cs
@@ -2,23 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StageRotationAxis : MonoBehaviour {
+public class StageRotationPivotAxis : MonoBehaviour {
     private float direction;
     private PlayerMove PlayerMove;
     private GameObject StageGroup;
+    [SerializeField] private GameObject Pivot;
     [SerializeField] private float startAngle;
     [SerializeField] private float lastAngle;
 
     void Start () {
-        StageGroup = GameObject.Find ("StageGroup");
+        StageGroup = GameObject.Find ("stage2");
         PlayerMove = GameObject.Find ("chr_robot").GetComponent<PlayerMove> ();
-
     }
     void Update () { }
 
     private void OnTriggerStay (Collider other) {
         if (other.gameObject.CompareTag ("Player")) {
-            transform.localScale = new Vector3 (10, 2, 10);
+            transform.localScale = new Vector3 (50, 3, 50);
             transform.parent = null;
             StageGroup.transform.parent = transform;
             float y = StageGroup.transform.rotation.eulerAngles.y;
@@ -27,27 +27,27 @@ public class StageRotationAxis : MonoBehaviour {
             if (direction > 0) {
                 if (startAngle <= y && y < lastAngle) {
                     PlayerMove.IsMoveFalse ();
-                    transform.Rotate (new Vector3 (0, 5, 0));
+                    transform.Rotate (new Vector3 (0, 1, 0));
                 } else {
                     transform.rotation = Quaternion.Euler (0, lastAngle, 0);
                     PlayerMove.IsMoveTrue ();
+                    StageGroup.transform.parent = null;
+                    transform.parent = StageGroup.transform.parent;
+                    transform.localScale = new Vector3 (20, 3, 20);
                 }
             } else if (direction < 0) {
                 if (startAngle + 1 < y && y <= lastAngle + 1) {
                     PlayerMove.IsMoveFalse ();
-                    transform.Rotate (new Vector3 (0, -5, 0));
+                    transform.Rotate (new Vector3 (0, -1, 0));
                 } else {
                     transform.rotation = Quaternion.Euler (0, startAngle, 0);
                     PlayerMove.IsMoveTrue ();
+                    StageGroup.transform.parent = null;
+                    transform.parent = StageGroup.transform;
+                    transform.localScale = new Vector3 (20, 3, 20);
                 }
             }
-            StageGroup.transform.parent = null;
-            transform.parent = StageGroup.transform;
 
         }
-    }
-    private void OnTriggerExit (Collider other) {
-        transform.localScale = new Vector3 (0.1f, 2, 0.1f);
-        PlayerMove.IsMoveTrue ();
     }
 }
