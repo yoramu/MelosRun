a95a10f7aea8f81e7d11a1d722c07af2e97298ee
yoramu <yoramuu@gmail.com>
2019-03-20 00:22:39 +0900

    風邪の強さを弱くした

--- Assets/Script/UpArea.cs
+++ Assets/Script/UpArea.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UpArea : MonoBehaviour {
-
+    [SerializeField] private float y;
     void Update () {
         // transform.localPosition = new Vector3 (transform.position.x, Mathf.PingPong (Time.time, 0.5f) + transform.position.y, transform.position.z);
     }
     private void OnTriggerStay (Collider other) {
         if (other.gameObject.CompareTag ("Player")) {
-            other.attachedRigidbody.AddForce (new Vector3 (0f, 1f, 0f), ForceMode.Impulse);
+            other.attachedRigidbody.AddForce (new Vector3 (0f, y, 0f), ForceMode.Impulse);
         }
     }
 }
