f8661bd291cf6f050eca0d0b6c31ea4bba6965bd
yoramu <yoramuu@gmail.com>
2019-03-22 17:39:15 +0900

    タイトルシーン追加

---
+++ Assets/Script/GoToStage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToStage : MonoBehaviour {
+    private void OnTriggerEnter (Collider other) {
+        if (other.gameObject.name == "chr_robot") {
+            SceneManager.LoadScene ("StageScene");
+        }
+    }
+}
