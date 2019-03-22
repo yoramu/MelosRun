96020c866b1a0fed5732fd9a8bf0627eb57e0301
yoramu <yoramuu@gmail.com>
2019-03-22 15:04:51 +0900

    死亡SEとBGM追加

---
+++ Assets/BGM.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MainSoundScript : MonoBehaviour {
+    public bool DontDestroyEnabled = true;
+    void Start () {
+        if (DontDestroyEnabled) {
+            // Sceneを遷移してもオブジェクトが消えないようにする
+            DontDestroyOnLoad (this);
+        }
+    }
+}
